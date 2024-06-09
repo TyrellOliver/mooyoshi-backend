dd02e3627bd1730be3198ed849523ae08ba3aea9
Tyrell <tyrellkamara-oliver@pursuit.org>
2024-06-09 02:04:09 -0400

    completed all of the routes for the usersController, need to do authorization next

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,8 +1,18 @@
 // Import Express
 const express = require("express");
 const users = express.Router();
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const secret = process.env.SECRET;
 
-const { createUser, getUsers } = require("../queries/users");
+const {
+  getUsers,
+  getUser,
+  createUser,
+  logInUser,
+  updateUser,
+  deleteUser,
+} = require("../queries/users");
 
 // Get all Users(for dev purposes)
 users.get("/", async (req, res) => {
@@ -14,12 +24,73 @@ users.get("/", async (req, res) => {
   }
 });
 
-// users.post("/", (req, res) =>{
-//   try {
+users.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(req.user);
+    const oneUser = await getUser(id);
+    res.status(200).json(oneUser);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+});
 
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// })
+users.post("/", async (req, res) => {
+  try {
+    const newUser = await createUser(req.body);
+    const token = jwt.sign(
+      { userID: newUser.user_id, username: newUser.username },
+      secret
+    );
+    res.status(201).json({ user: newUser, token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
+users.post("/login", async (req, res) => {
+  try {
+    const loggedInUser = await logInUser(req.body);
+    if (!loggedInUser) {
+      res.status(401).json({ error: "Invalid username or password" });
+      return;
+    }
+
+    const token = jwt.sign(
+      { userID: loggedInUser.user_id, username: loggedInUser.username },
+      secret
+    );
+
+    res.status(200).json({
+      user: {
+        user_id: loggedInUser.user_id,
+        username: loggedInUser.username,
+        email: loggedInUser.email,
+      },
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+users.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedUser = await updateUser(req.body, id);
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+users.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await deleteUser(id);
+    res.status(200).json({ success: "Successfully deleted profile" });
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+});
 module.exports = users;
--- queries/users.js
+++ queries/users.js
@@ -5,15 +5,38 @@ const db = require("../db/dbConfig");
 const bcrypt = require("bcrypt");
 
 // Queries for the database
+// For dev purposes
+const getUsers = async () => {
+  try {
+    const allUsers = await db.any("SELECT * FROM users");
+    // console.log(allUsers);
+    return allUsers;
+  } catch (error) {
+    // console.log(error);
+    return error;
+  }
+};
+
+const getUser = async (id) => {
+  try {
+    const oneUser = await db.one("SELECT * FROM users WHERE user_id=$1", id);
+    // console.log(oneUser);
+    return oneUser;
+  } catch (error) {
+    // console.log(error);
+    return error;
+  }
+};
+
 const createUser = async (user) => {
   try {
-    const { username, email, password_hash } = profile;
+    const { username, email, password_hash } = user;
     const salt = 10;
     const hash = await bcrypt.hash(password_hash, salt);
 
     const newUser = await db.one(
-      "INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3) RETURNING *",
-      [username, email, password_hash]
+      "INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3) RETURNING user_id, username, email, created_at, updated_at",
+      [username, email, hash]
     );
     console.log(newUser);
     return newUser;
@@ -23,16 +46,64 @@ const createUser = async (user) => {
   }
 };
 
-// For dev purposes
-const getUsers = async () => {
+const logInUser = async (user) => {
   try {
-    const allUsers = await db.any("SELECT * FROM users");
-    console.log(allUsers);
-    return allUsers;
+    const loggedInUser = await db.oneOrNone(
+      "SELECT * FROM users WHERE username=$1",
+      user.username
+    );
+    if (!loggedInUser) {
+      return false;
+    }
+    const passwordMatch = await bcrypt.compare(
+      user.password_hash,
+      loggedInUser.password_hash
+    );
+    if (!passwordMatch) {
+      return false;
+    }
+    // console.log(loggedInUser);
+    return loggedInUser;
   } catch (error) {
-    console.log(error);
+    // console.log(error);
     return error;
   }
 };
 
-module.exports = { createUser, getUsers };
+const updateUser = async (user, id) => {
+  try {
+    const { username } = user;
+    const updatedUser = await db.one(
+      "UPDATE users SET username=$1 WHERE user_id=$2 RETURNING *",
+      [username, id]
+    );
+    // console.log(updatedUser);
+    return updatedUser;
+  } catch (error) {
+    // console.log(error);
+    return error;
+  }
+};
+
+const deleteUser = async (id) => {
+  try {
+    const deletedUser = await db.one(
+      "DELETE FROM users WHERE user_id=$1 RETURNING *",
+      id
+    );
+    // console.log(deletedUser);
+    return deletedUser;
+  } catch (error) {
+    // console.log(error);
+    return error;
+  }
+};
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+  logInUser,
+  updateUser,
+  deleteUser,
+};
